fd27f8e3e2f8ea0cc9a1200fc8d1dffb030435e2
Francois Jeannotte <francois.jeannotte@gmail.com>
2012-12-16 19:55:58 -0500

    Add test

--- test.cpp
+++ test.cpp
@@ -6,19 +6,55 @@
 
 using namespace std;
 
+DWORD WINAPI producer(LPVOID param) 
+{
+  ulock::mtstack<int>* stack = static_cast<ulock::mtstack<int>*>(param);
+  for (int i = 0; i < 10000; ++i)
+  {
+    stack->push(i);
+    //Sleep(1);
+  }
+  return 0;
+}
+
+DWORD WINAPI consumer(LPVOID param)
+{
+  ulock::mtstack<int>* stack = static_cast<ulock::mtstack<int>*>(param);
+  int pop_count = 0;
+  int val = 0;
+  while (pop_count < 9999)
+  {
+    if (stack->pop(val))
+    {
+      ++pop_count;
+    }
+  }
+
+  return 0;
+}
+
 int main()
 {
   ulock::mtstack<int> stack;
 
-  stack.push(1);
-  stack.push(2);
-  stack.push(3);
-  stack.push(4);
-  stack.push(5);
-  int val = 0;
-  while (stack.pop(val))
-  {
-    cout << val << endl;
-  }
+  LARGE_INTEGER begin;
+  QueryPerformanceCounter(&begin);
+
+  cout << "Launch producer threads" << endl;
+  CreateThread(NULL, 0, producer, &stack, 0, NULL);
+  CreateThread(NULL, 0, producer, &stack, 0, NULL);
+  CreateThread(NULL, 0, producer, &stack, 0, NULL);
+  CreateThread(NULL, 0, producer, &stack, 0, NULL);
+  cout << "Launch consumer thread" << endl;
+  HANDLE h = CreateThread(NULL, 0, consumer, &stack, 0, NULL);
+  cout << "Wait for consumer thread to finish" << endl;
+  WaitForSingleObject(h, 5000);
+
+  LARGE_INTEGER end;
+  QueryPerformanceCounter(&end);
+  LARGE_INTEGER freq;
+  QueryPerformanceFrequency(&freq);
+  cout << double(end.QuadPart - begin.QuadPart) / freq.QuadPart << " seconds" << endl;
+
   return 0;
 }
